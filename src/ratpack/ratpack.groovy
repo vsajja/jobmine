178964e8532a31c62539e4cfb477efc638bb1af0
Vinod Sajja <Vinod Sajja>
2016-10-25 04:00:31 -0600

    jobs endpoint

--- src/ratpack/ratpack.groovy
+++ src/ratpack/ratpack.groovy
@@ -1,6 +1,15 @@
+import jooq.tables.JobPosting
+import jooq.tables.daos.JobPostingDao
+import org.jooq.Configuration
+import org.jooq.SQLDialect
+import org.jooq.impl.DefaultConfiguration
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import vsajja.org.redis.RedisConfig
+import javax.sql.DataSource
 
 import static ratpack.groovy.Groovy.ratpack
+import static ratpack.jackson.Jackson.json;
 
 import vsajja.org.postgres.PostgresConfig
 import vsajja.org.postgres.PostgresModule
@@ -8,9 +17,13 @@ import ratpack.config.ConfigData
 import ratpack.config.ConfigDataBuilder
 import ratpack.groovy.sql.SqlModule
 import ratpack.hikari.HikariModule
+import ratpack.handling.RequestLogger
+
 import com.zaxxer.hikari.HikariConfig
 import groovy.sql.Sql
 import java.text.SimpleDateFormat
+
+final Logger log = LoggerFactory.getLogger(this.class)
 
 ratpack {
     bindings {
@@ -36,9 +49,13 @@ ratpack {
                             configData.get('/postgres', PostgresConfig))
         }
         module SqlModule
+
+        bind JobPostingDao
     }
 
     handlers {
+        all RequestLogger.ncsa(log)
+
         get {
             println "/ ${new SimpleDateFormat('yyyy-MM-dd HH:mm:ss.SSS').format(new Date())}"
             render "Hello world!"
@@ -49,9 +66,18 @@ ratpack {
             render "Hello world!"
         }
 
-        get("db") { Sql sql ->
-            def schemas = sql.rows("select * from public.user")
-            render schemas?.join(' ')
+        get('jobs') {
+            DataSource dataSource = registry.get(DataSource.class);
+            Configuration configuration = new DefaultConfiguration().set(dataSource).set(SQLDialect.POSTGRES)
+
+//            DSLContext create = DSL.using(dataSource, SQLDialect.POSTGRES)
+//            SelectJoinStep<Record> from =create.select().from(JobPosting.JOB_POSTING);
+//            Promise<List<Map<String, Object>>> promise = ratpack.exec.Blocking.get(() -> from.fetch().intoMaps());
+
+            List<JobPosting> jobPostings = new JobPostingDao(configuration).findAll()
+            render json(jobPostings)
+//            def schemas = sql.rows("select * from public.user")
+//            render schemas?.join(' ')
         }
 
         files {
