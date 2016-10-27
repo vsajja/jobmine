b0c7994eae8b22bfc08a0449a1fdf62be084fd2d
Vinod Sajja <Vinod Sajja>
2016-10-27 00:51:49 -0600

    fix prod

--- ui/app/scripts/app.js
+++ ui/app/scripts/app.js
@@ -43,5 +43,5 @@ angular
 
     // TODO: for dev point to Ratpack server
     RestangularProvider.setBaseUrl('/api/v1');
-     //RestangularProvider.setBaseUrl('http://localhost:5050/api/v1');
-  });
+    // RestangularProvider.setBaseUrl('http://localhost:5050/api/v1');
+  });clear
