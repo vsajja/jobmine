de829bddb303eadb811784efb523f8198097db4e
Vinod Sajja <Vinod Sajja>
2016-10-28 06:39:31 -0600

    profile updates

--- ui/app/scripts/controllers/profile.js
+++ ui/app/scripts/controllers/profile.js
@@ -8,10 +8,25 @@
  * Controller of the jobmineApp
  */
 angular.module('jobmineApp')
-  .controller('ProfileCtrl', function () {
+  .controller('ProfileCtrl', function ($scope, $window) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+    $scope.tabs = [
+      { title:'Dynamic Title 1', content:'Dynamic content 1' },
+      { title:'Dynamic Title 2', content:'Dynamic content 2', disabled: true }
+    ];
+
+    $scope.alertMe = function() {
+      setTimeout(function() {
+        $window.alert('You\'ve selected the alert tab!');
+      });
+    };
+
+    $scope.model = {
+      name: 'Tabs'
+    };
   });
+
