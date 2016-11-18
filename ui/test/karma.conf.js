3c358f08327b60fc9758ad06fec6f121e1195813
Vinod Sajja <Vinod Sajja>
2016-11-18 00:56:45 -0700

    add angular x-editable

--- ui/test/karma.conf.js
+++ ui/test/karma.conf.js
@@ -33,6 +33,7 @@ module.exports = function(config) {
       'bower_components/angular-smart-table/dist/smart-table.js',
       'bower_components/lodash/lodash.js',
       'bower_components/restangular/dist/restangular.js',
+      'bower_components/angular-xeditable/dist/js/xeditable.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
