8720d673edd979769bc8fb6849e2a19b3353654d
Vinod Sajja <Vinod Sajja>
2016-11-19 05:21:53 -0700

    progress;

--- ui/app/scripts/app.js
+++ ui/app/scripts/app.js
@@ -118,8 +118,8 @@ var jobmineApp = angular
       });
 
     // TODO: for dev point to Ratpack server
-    RestangularProvider.setBaseUrl('/api/v1');
-    // RestangularProvider.setBaseUrl('http://localhost:5050/api/v1');
+    // RestangularProvider.setBaseUrl('/api/v1');
+    RestangularProvider.setBaseUrl('http://localhost:5050/api/v1');
   });
 
 jobmineApp.run(function (editableOptions) {
