f2e5e0718d8469f52fad85e768ad6b298d177ec2
Vinod Sajja <Vinod Sajja>
2016-11-20 06:07:47 -0700

    compress the backup silly

--- db/backups/nov20/d4ogiv1q9mi0tp_public_job_mine.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO public.job_mine (job_mine_id, name, school_id) VALUES (81, 'jobmine', null);
-INSERT INTO public.job_mine (job_mine_id, name, school_id) VALUES (71, 'University of Waterloo''s jobmine', null);
