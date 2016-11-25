959a4a3c6cb1f977ab55550f3c693c0df870154d
Vinod Sajja <Vinod Sajja>
2016-11-24 20:35:05 -0700

    progress

---
+++ db/backups/nov_23/d4ogiv1q9mi0tp_public_job_mine.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.job_mine (job_mine_id, name, school_id) VALUES (81, 'jobmine', null);
+INSERT INTO public.job_mine (job_mine_id, name, school_id) VALUES (71, 'University of Waterloo''s jobmine', null);
