99e308ea91991b6f70092c45e24449c7d670f41e
vsajja <vinod.sajja@gmail.com>
2017-12-22 11:46:02 -0700

    progress

--- uia4/src/environments/environment.prod.ts
+++ uia4/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ import * as env from './.env.json';
 export const environment = {
   production: true,
   version: env.npm_package_version,
-  serverUrl: 'https://www.jobmine.ca',
+  serverUrl: 'https://jm-app.herokuapp.com',
   apiUrl: '/api/v1',
   defaultLanguage: 'en-US',
   supportedLanguages: [
